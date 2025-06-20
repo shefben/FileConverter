b71865c4ac4c9560c14f78c8a88c5d92fd805938
shefben <shefben@gmail.com>
2025-06-20 03:24:18 -0500

    Fix csproj format

---
+++ Application/FileConverter/Views/ManageCustomConvertersWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace FileConverter.Views
+{
+    public partial class ManageCustomConvertersWindow : Window
+    {
+        public ManageCustomConvertersWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
